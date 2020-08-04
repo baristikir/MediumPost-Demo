400ad9b290858110ebbc4542bf50903438d093cc
Baris Tikir <brstkr@icloud.com>
2020-08-04 19:41:33 +0200

    Initial Commit

---
+++ Medium_Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Medium_Demo
+//
+//  Created by Baris Tikir on 04.08.20.
+//  Copyright © 2020 Baris Tikir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
